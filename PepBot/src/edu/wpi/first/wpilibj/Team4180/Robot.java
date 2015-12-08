6fcfdde6ea622dc0c46e648479947b338f261b43
Iron Rider <studentleaders@ironriders.org>
2015-12-08 14:30:07 -0800

    Created the netbeans project
    there is no code in it other than the premade code

--- PepBot/src/edu/wpi/first/wpilibj/Team4180/Robot.java
+++ PepBot/src/edu/wpi/first/wpilibj/Team4180/Robot.java
@@ -1,5 +1,12 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team4180.robot;
+package edu.wpi.first.wpilibj.Team4180;
+
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
