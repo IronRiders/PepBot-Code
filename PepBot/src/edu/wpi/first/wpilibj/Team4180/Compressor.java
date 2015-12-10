362a1462f2cdb711590ee721206ad3ff5b7dacd6
alexmaclean <alex.maclean2000@gmail.com>
2015-12-09 16:08:40 -0800

    Removed redundant code and added comments

--- PepBot/src/edu/wpi/first/wpilibj/Team4180/Compressor.java
+++ PepBot/src/edu/wpi/first/wpilibj/Team4180/Compressor.java
@@ -4,53 +4,59 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Relay;
 
 public class Compressor {
-        //Collection of Booleans tracking the state/wanted state
-	private boolean WantCompressorOn;
-	private boolean PressureWasMax;
-	private boolean PressureIsMax;
-	private boolean CompressorIsOn;
-        
-        //TODO: Add a comment for the relay
-	private Relay Commpressing;
-        
-        //The port the Compressor is in and the DI used to get info from it
-	private int CompressorPort;
-	private DigitalInput DI;
-	
-	
-	//Constructor that initializes its port number and and the DigitalInput
-	public Compressor(int portNumber) {
-		Integer numberPort = Integer.valueOf(portNumber);
-		CompressorPort = numberPort.intValue();
-		System.out.println("Attached Switch Listener to port " + numberPort);
-		DI = new DigitalInput(CompressorPort);
-	}
-        
-	//
-	public void listen(){
-		PressureWasMax = PressureIsMax;
-		PressureIsMax = DI.get();
-		if(PressureIsMax !=	PressureWasMax){
-			CompressorIsOn = getState(WantCompressorOn, PressureIsMax);
-		}
-	}
-	/**
-	 * Changes the desired state variable to the boolean passed in
-	 * Determines whether to allow continued compression
-	 * @param Desired State
-	 */
-	public void setDesiredState(boolean b) {
-		WantCompressorOn = b;
-		CompressorIsOn = getState(WantCompressorOn, PressureIsMax);
-	}
-	/**
-	 * 
-	 * @param Desired compressor state
-	 * @param is the Air tank at the maximum pressure?
-	 * @return
-	 */
-	private boolean getState(boolean want, boolean max){
-		if(!max) return want;
-		else return !max;
-	}
+
+    //Collection of Booleans tracking the state/wanted state
+    private boolean WantCompressorOn;
+    private boolean PressureWasMax;
+    private boolean PressureIsMax;
+    //private boolean CompressorIsOn;
+
+    //This relay turns the Compressor on the 
+    private Relay Compressing;
+    //The port the Compressor is in and the DI used to get info from it
+    private DigitalInput DI;
+
+    //Constructor that initializes its port number and and the DigitalInput
+    public Compressor(int compPortNumber, int inputPort) {
+        Compressing = new Relay(compPortNumber);
+        DI = new DigitalInput(inputPort);
+    }
+
+    //
+    public void listen() {
+        PressureWasMax = PressureIsMax;
+        PressureIsMax = DI.get();
+        if (PressureIsMax != PressureWasMax) 
+            changeCompressing();
+    }
+
+    /**
+     * Changes the desired state variable to the boolean passed in Determines
+     * whether to allow continued compression
+     *
+     * @param Desired State
+     */
+    public void setDesiredState(boolean b) {
+        WantCompressorOn = b;
+        changeCompressing();
+    }
+
+    /**
+     *
+     * @param want Desired compressor state
+     * @param max is the Air tank at the maximum pressure?
+     * @return
+     */
+    private boolean getState(boolean want, boolean max) {
+        if (!max) {
+            return want;
+        } else {
+            return !max;
+        }
+    }
+    private void changeCompressing(){
+       boolean b = getState(WantCompressorOn, PressureIsMax);
+        if(b) Compressing.set(Relay.Value.kOn); 
+        else  Compressing.set(Relay.Value.kOff); 
+    }
 }
