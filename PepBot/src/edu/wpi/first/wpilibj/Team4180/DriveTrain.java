4c2b33e4a48faf7cb1c9f264291583193fc4aa24
Fiona Shaffer <>
2015-12-10 16:05:02 -0800

    All of the methods written for DriveTrain

--- PepBot/src/edu/wpi/first/wpilibj/Team4180/DriveTrain.java
+++ PepBot/src/edu/wpi/first/wpilibj/Team4180/DriveTrain.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.Team4180;
 
 import edu.wpi.first.wpilibj.Jaguar;
@@ -13,23 +12,51 @@ import edu.wpi.first.wpilibj.Jaguar;
  * @author kwruff
  */
 public class DriveTrain {
+
     //Declaring motors
-    Jaguar leftJag1;
-    Jaguar leftJag2;
-    Jaguar rightJag1;
-    Jaguar rightJag2;
-    
-    double xPos;
-    double yPos;
-    //double speed;
-    
-    public DriveTrain(){
+    private Jaguar leftJag1;
+    private Jaguar leftJag2;
+    private Jaguar rightJag1;
+    private Jaguar rightJag2;
+
+    private double xPos;
+    private double yPos;
+
+    public DriveTrain() {
         leftJag1 = new Jaguar(1);
         leftJag2 = new Jaguar(2);
         rightJag1 = new Jaguar(3);
         rightJag2 = new Jaguar(4);
-        //double xPos;
-        //double yPos;
+        xPos = 0.0;
+        yPos = 0.0;
+    }
+
+    public void setLeftJagSpeed(double leftJagSpeed) {
+        leftJag1.set(leftJagSpeed);
+        leftJag2.set(leftJagSpeed);
+    }
+
+    public void setRightJagSpeed(double rightJagSpeed) {
+        rightJag1.set(rightJagSpeed);
+        rightJag2.set(rightJagSpeed);
+    }
+
+    public void setJagspeed(double leftJagSpeed, double rightJagSpeed) {
+        setLeftJagSpeed(leftJagSpeed);
+        setRightJagSpeed(rightJagSpeed);
+    }
+
+    public void updateXPos(double newXPos) {
+        xPos = newXPos;
+    }
+
+    public void updateYPos(double newYPos) {
+        yPos = newYPos;
+    }
+
+    public void updateSpeed(){
+    setJagspeed(Math.min((Math.max(-1, yPos+xPos)),1),
+            Math.min((Math.max(-1, yPos-xPos)),1));
     }
     
     //TODO: method that sets rightJag speeds
