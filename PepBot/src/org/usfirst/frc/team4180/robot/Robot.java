af8e1e171f6167ae5d220bb6d86b2e987fdbbe4a
Iron Rider <studentleaders@ironriders.org>
2015-12-14 15:06:34 -0800

    Switched back to Eclipse
    thanks hardware...

--- PepBot/src/org/usfirst/frc/team4180/robot/Robot.java
+++ PepBot/src/org/usfirst/frc/team4180/robot/Robot.java
@@ -1,12 +1,5 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
-package edu.wpi.first.wpilibj.Team4180;
-
+package org.usfirst.frc.team4180.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
